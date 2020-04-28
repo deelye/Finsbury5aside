fe513a171c438b3553c0d3d18b4726e17ba36b21
deelye <dee.yen.lye@gmail.com>
2020-04-28 12:47:59 +0100

    added associations and validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,6 @@
 class Game < ApplicationRecord
+  has_many :lineups, dependent: :destroy
+  has_many :players, through: :lineups
+
+  validates :date, presence: true, uniquess: true
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,6 @@
 class Player < ApplicationRecord
+  has_many :lineups
+  has_many :games, through: :lineups
+
+  validates :name, presence: true, uniqueness: true
 end
