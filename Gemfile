7cf4fd1f53936add88c6cd3c1e2ce11c177dd468
deelye <dee.yen.lye@gmail.com>
2020-04-29 18:35:49 +0100

    removed date validation; added datepicker gem

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'sassc-rails'
 gem 'simple_form'
 gem 'uglifier'
 gem 'webpacker'
+gem 'bootstrap-datepicker-rails', '~> 1.8', '>= 1.8.0.1'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,19 @@
 class GamesController < ApplicationController
   def new
     @games = Game.all
+    @game = Game.new
+  end
+
+  def create
+    @games = Game.all
+
+    date = Date.parse(game_params[:date])
+    @game = Game.new(date: date)
+    if @game.save
+      redirect_to new_game_path
+    else
+      render :new
+    end
   end
 
   def destroy
@@ -8,4 +21,11 @@ class GamesController < ApplicationController
     @game.destroy
     redirect_to new_game_path
   end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:date)
+  end
+
 end
