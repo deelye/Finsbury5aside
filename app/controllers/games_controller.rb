dc6e2a9896c33ab5cf4e082470427397e3555e3f
deelye <dee.yen.lye@gmail.com>
2020-04-29 15:05:13 +0100

    added games controller, new view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,11 @@
 class GamesController < ApplicationController
+  def new
+    @games = Game.all
+  end
 
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+    redirect_to new_game_path
+  end
 end
